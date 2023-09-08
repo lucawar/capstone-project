a0b970c0e7ed98a2b16bbc9f604f53eaaa4fc4b5
lucawar <lucaguerra92@yahoo.it>
2023-09-08 15:59:28 +0200

    cambiato url del frontend

--- src/main/java/lucaguerra/security/CorsFilter.java
+++ src/main/java/lucaguerra/security/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Allow-Headers", "*");
 		response.setHeader("Access-Control-Allow-Max-Age", "3600");
